2c30380690f52c6b9f1ed425bcaa9f903df8cdee
ru1313 <lin123252@gmail.com>
2024-12-16 22:33:48 +0700

    6

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -26,12 +26,12 @@ const resources = {
   svgSprite: "src/assets/svg-sprite/*.svg",
   static: [
     "src/assets/icons/**/*.*",
-   // "src/assets/favicons/**/*.*",
+    //"src/assets/favicons/**/*.*",
     "src/assets/fonts/**/*.{woff,woff2}",
     //"src/assets/video/**/*.{mp4,webm}",
     //"src/assets/audio/**/*.{mp3,ogg,wav,aac}",
     //"src/json/**/*.json",
-   // "src/php/**/*.php"
+    //"src/php/**/*.php"
   ]
 };
 // Gulp Tasks:
@@ -103,15 +103,15 @@ function copy() {
 }
 function images() {
   return gulp
-    .src(resources.images)
+    .src(resources.images, { encoding: false })
     .pipe(
       imagemin([
         imagemin_gifsicle({ interlaced: true }),
         imagemin_mozjpeg({ quality: 100, progressive: true }),
-        imagemin_optipng({ optimizationLevel: 3 })
+        imagemin_optipng({ optimizationLevel: 5 })
       ])
     )
-    .pipe(gulp.dest("dist/assets/images"));
+    .pipe(gulp.dest('dist/assets/images'));
 }
 function svgSprite() {
   return gulp
